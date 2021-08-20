2ca5bcd7a339a659308af952c253a5c87a8b7206
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2021-08-20 08:51:34 +0200

    Add the setup.py file to build with cx_Freeze

---
+++ setup.py
@@ -0,0 +1,28 @@
+﻿import distutils
+import opcode
+import os
+
+from cx_Freeze import setup, Executable
+
+campi = Executable(
+    script="src/campi/main.py",
+    base="Win32GUI",
+    target_name="campi"
+)
+
+includefiles = [
+        ("src/campi/main.bui", "src/campi/main.bui"),
+]
+setup(
+    name = "campi",
+    version = "0.1",
+    description = "The Campi Feed Reader.",
+    options = {'build_exe': {
+            "include_files": includefiles,
+            "excludes": ["_gtkagg", "_tkagg", "bsddb", "distutils", "curses",
+                    "pywin.debugger", "pywin.debugger.dbgcon",
+                    "pywin.dialogs", "tcl", "Tkconstants", "Tkinter"],
+            "packages": ["bui.specific.wx4"],
+    }},
+    executables = [campi]
+)
